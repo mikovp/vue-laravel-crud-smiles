95d7da5bbba36e298a1941e55475ab051936324b
Mikhail A <mikemiha@outlook.com>
2021-02-02 22:46:17 +0300

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>Smiles</title>
 </head>
 <header>
@@ -19,6 +23,5 @@
 <div id="app">
     <app></app>
 </div>
-<script src="js/app.js"></script>
 </body>
 </html>
