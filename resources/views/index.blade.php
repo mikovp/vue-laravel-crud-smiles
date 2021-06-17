24812a222082cf5224b2f69c73d7d60048c34c00
Mikhail A <mikemiha@outlook.com>
2021-06-17 16:02:41 +0300

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,9 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="js/app.js" defer></script>
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="css/app.css" rel="stylesheet">
     <title>Smiles</title>
 </head>
 <header>
